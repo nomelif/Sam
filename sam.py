fe43025a5cf64c375df7170567ecb85c8ca077a5
Théo Friberg <theo.friberg@gmail.com>
2016-11-08 17:47:47 +0200

    Main executable

---
+++ sam.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""
+
+This is the main launchable file for Sam. It reads input from `InputDevices` and works as a relay between scripts and `OutputDevices`.
+
+"""
+
+from SoundInterface import InputDevice, OutputDevice
